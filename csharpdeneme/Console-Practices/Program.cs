d4ae27145f69b5049b71e8bab73f5c9eb4263f98
Türkü Meydan <78116039+meydanturku@users.noreply.github.com>
2021-11-24 22:28:02 +0300

    Add files via upload

---
+++ csharpdeneme/Console-Practices/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace consoleprogrammingtester
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girin");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba" + name+ " " + surname);
+        }
+    }
+}
